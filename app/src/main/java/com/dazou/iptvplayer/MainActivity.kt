b1858d8f21cd56aaadb3588ff6f84235a4e57280
awsnuaimi <aws.nuaimi@gmail.com>
2026-07-12 23:10:53 +0200

    update

--- app/src/main/java/com/dazou/iptvplayer/MainActivity.kt
+++ app/src/main/java/com/dazou/iptvplayer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.dazou.iptvplayer
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.dazou.iptvplayer.databinding.ActivityMainBinding
 import com.google.android.exoplayer2.ExoPlayer
 import com.google.android.exoplayer2.MediaItem
@@ -13,12 +12,16 @@ class MainActivity : AppCompatActivity() {
     private var player: ExoPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        installSplashScreen()
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
         val prefs = getSharedPreferences("IPTV_Prefs", MODE_PRIVATE)
+        val savedUrl = prefs.getString("url", "")
+
+        if (!savedUrl.isNullOrEmpty()) {
+            binding.etUrl.setText(savedUrl)
+        }
 
         binding.btnConnect.setOnClickListener {
             val url = binding.etUrl.text.toString()
