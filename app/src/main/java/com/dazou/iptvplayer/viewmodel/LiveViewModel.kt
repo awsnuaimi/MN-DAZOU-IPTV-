a8e05c55a9fb93fa9f160e8568953902349d3605
awsnuaimi <aws.nuaimi@gmail.com>
2026-07-15 03:35:28 +0200

    update LiveViewModel.kt

--- app/src/main/java/com/dazou/iptvplayer/viewmodel/LiveViewModel.kt
+++ app/src/main/java/com/dazou/iptvplayer/viewmodel/LiveViewModel.kt
@@ -1,29 +1,156 @@
 package com.dazou.iptvplayer.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.Observer
+import com.dazou.iptvplayer.data.AccountManager
 import com.dazou.iptvplayer.data.XtreamRepository
 import com.dazou.iptvplayer.model.XtreamChannel
 import com.dazou.iptvplayer.model.XtreamServer
 
-class LiveViewModel : ViewModel() {
+
+class LiveViewModel(
+    application: Application
+) : AndroidViewModel(application) {
+
 
     private var repository: XtreamRepository? = null
-    private val _channels = MutableLiveData<List<XtreamChannel>>()
-    val channels: LiveData<List<XtreamChannel>> = _channels
 
-    fun setServer(server: XtreamServer) {
-        repository = XtreamRepository(server)
-    }
+
+    private val accountManager =
+        AccountManager(application)
+
+
+
+    private val _channels =
+        MutableLiveData<List<XtreamChannel>>()
+
+
+    val channels: LiveData<List<XtreamChannel>>
+        get() = _channels
+
+
+
+    private var channelsObserver:
+            Observer<List<XtreamChannel>>? = null
+
+
+
+
 
     fun loadAllChannels() {
-        repository?.loadLiveStreams()
-        repository?.liveChannels?.observeForever { _channels.postValue(it) }
+
+
+        val server =
+            accountManager.getActiveAccount()
+
+
+
+        if (server == null) {
+
+
+            _channels.value =
+                emptyList()
+
+
+            return
+
+        }
+
+
+
+        repository =
+            XtreamRepository(server)
+
+
+
+
+        channelsObserver =
+            Observer {
+
+
+                _channels.postValue(it)
+
+
+            }
+
+
+
+
+        repository!!
+            .liveChannels
+            .observeForever(
+                channelsObserver!!
+            )
+
+
+
+        repository!!
+            .loadLiveStreams()
+
+
     }
 
-    fun getServer(): XtreamServer {
-        // TODO: استرجاع السيرفر من SharedPreferences
-        return XtreamServer("", "", "")
+
+
+
+
+
+
+    fun getServer(): XtreamServer? {
+
+
+        return accountManager
+            .getActiveAccount()
+
+
     }
+
+
+
+
+
+
+
+    fun setServer(
+        server: XtreamServer
+    ) {
+
+
+        repository =
+            XtreamRepository(server)
+
+
+    }
+
+
+
+
+
+
+
+
+    override fun onCleared() {
+
+
+        super.onCleared()
+
+
+
+        channelsObserver?.let {
+
+
+            repository
+                ?.liveChannels
+                ?.removeObserver(it)
+
+
+        }
+
+
+    }
+
+
 }
