7a3cd157a857d420a0699128a6a17437548efc02
awsnuaimi <aws.nuaimi@gmail.com>
2026-07-16 14:50:24 +0200

    update App.kt

--- app/src/main/java/com/dazou/iptvplayer/App.kt
+++ app/src/main/java/com/dazou/iptvplayer/App.kt
@@ -2,7 +2,14 @@ package com.dazou.iptvplayer
 
 import android.app.Application
 import com.dazou.iptvplayer.data.AppContainer
+import com.dazou.iptvplayer.utils.CrashHandler
 
 class App : Application() {
+
     val container by lazy { AppContainer(this) }
+
+    override fun onCreate() {
+        super.onCreate()
+        CrashHandler.install(this)
+    }
 }
