cdc5e76c2ef0924294ef7e95bd831088dc81303b
awsnuaimi <aws.nuaimi@gmail.com>
2026-07-19 05:04:36 +0200

    update

--- app/src/main/java/com/dazou/iptvplayer/App.kt
+++ app/src/main/java/com/dazou/iptvplayer/App.kt
@@ -1,6 +1,7 @@
 package com.dazou.iptvplayer
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import com.dazou.iptvplayer.data.AppContainer
 import com.dazou.iptvplayer.utils.CrashHandler
 
@@ -11,5 +12,14 @@ class App : Application() {
     override fun onCreate() {
         super.onCreate()
         CrashHandler.install(this)
+        applySavedTheme()
+    }
+
+    private fun applySavedTheme() {
+        val prefs = getSharedPreferences("dazou_prefs", MODE_PRIVATE)
+        val isLight = prefs.getBoolean("is_light_theme", false)
+        AppCompatDelegate.setDefaultNightMode(
+            if (isLight) AppCompatDelegate.MODE_NIGHT_NO else AppCompatDelegate.MODE_NIGHT_YES
+        )
     }
 }
